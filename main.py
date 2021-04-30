728b3d9d9f83a21d9f0a34fa25c028b77098dac4
dp.sokolov <dp.sokolov@corp.mail.ru>
2021-04-30 09:52:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('DevMan')
